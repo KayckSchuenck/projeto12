0b1496f7a7961cb440fbaf9126f82c59a9418d1f
KayckSchuenck <ycferreiras@gmail.com>
2022-06-27 17:42:05 -0300

    Versao final

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express,{json} from 'express'
 import cors from 'cors'
 import {MongoClient} from 'mongodb'
 import dotenv from 'dotenv'
-import dayjs from 'dayjs/locale/pt-br'
+import dayjs from 'dayjs'
 import joi from 'joi'
 
 dotenv.config()
@@ -13,101 +13,122 @@ const server=express()
 server.use(cors())
 server.use(json())
 
+client.connect().then(()=>{
+    db=client.db('API_UOL')
+})
+
 server.post('/participants',async(req,res)=>{
     try{
-        await client.connect()
-        db=client.db('API_UOL')
+        const schemaParticipants=joi.object({
+            name:joi.string().required(),
+        })
+        const validation=schemaParticipants.validate(req.body)
+        if(validation.error){
+            res.sendStatus(422)
+            return
+        }
         const {name}=req.body
+        const check=await db.collection("participantes").findOne({name})
+        if(check){
+            res.sendStatus(409)
+            return
+        }
         await db.collection("participantes").insertOne({name,lastStatus:Date.now()})
         res.sendStatus(201)
-        client.close()
     } catch(e){
-        res.sendStatus(500)
-        client.close()
+        res.status(500).send(e)
     }
 })    
     
 server.get('/participants',async (req,res)=>{
     try {
-        await client.connect()
-        db=client.db('API_UOL')
         const participantes= await db.collection('participantes').find().toArray()
         res.send(participantes)
-        client.close()
     } catch(e){
-        res.sendStatus(500)
-        client.close()
+        res.status(500).send(e)
     }
 })
+
 server.post('/messages',async (req,res)=>{
+    const schemaMessages=joi.object({
+        to:joi.string().required(),
+        text:joi.string().required(),
+        type:joi.string().valid("message","private_message").required(),
+    })
+    const validation=schemaMessages.validate(req.body)
+    const {to,text,type}=req.body
+    const from=req.headers.user
+    const time=dayjs().format('HH:mm:ss')
+    const check=await db.collection("participantes").findOne({name:from})
     try {
-        await client.connect()
-        db=client.db('API_UOL')
-        const {to,text,type}=req.body
-        const from=req.headers.User
-        const time=dayjs().locale('pt-br').format('HH:MM:SS')
-        await db.collection('mensagem').insertOne({to,text,type,from,time})
+        if(!check||validation.error){
+            res.sendStatus(422)
+            return
+        }
+        await db.collection('mensagens').insertOne({
+            to,
+            text,
+            type,
+            from,
+            time
+        })
         res.sendStatus(201)
-        client.close()
     } catch(e){
-        res.sendStatus(500)
-        client.close()
+        res.status(500).send(e)
     }
 })
 server.get('/messages',async (req,res)=>{
     try{
-        await client.connect()
-        db=client.db('API_UOL')
-        const limit=parseInt(req.query.limit)
-        const {User}=req.headers
+        const limit=(req.query.limit)
+        const {user}=req.headers
         const mensagens= await db.collection('mensagens').find().toArray()
-        mensagens.filter(e=>e.to===User||e.from===User)
+        let filtrado=mensagens.filter(e=>e.to===user||e.from===user||e.type==='message')
         if(!limit){
-            res.send(mensagens)
+            res.send(filtrado)
         } else{
-            res.send(mensagens).splice(-limit)
+            res.send(filtrado.slice(-limit))
         }
-        client.close()
     } catch(e){
-        res.sendStatus(500)
-        client.close()
+      res.status(500).send(e)
     }
 })
 server.post('/status',async (req,res)=>{
     try{
-        await client.connect()
-        db=client.db('API_UOL')
-        const {User}=req.headers
-        const colecao=await db.collection('participantes')
-        const search=colecao.findOne({name:User})
+        const {user}=req.headers
+        const search= await db.collection('participantes').findOne({name:user})
         if(search){
-            await colecao.updateOne(
-                { name:User },
+            await db.collection('participantes').updateOne(
+                { name:user },
                 { $set:{lastStatus:Date.now()} }
             )
             res.sendStatus(200)
         } else{
             res.sendStatus(404)
-            client.close()
         }
-    } catch(e) {
-        res.sendStatus(500)
-        client.close()
+    } catch(e){
+        res.status(500).send(e)
     }
 })
 
 setInterval(async () => {
     try{
-        await client.connect()
-        db=client.db('API_UOL')
-        const query={$where:(e)=>{
-            Date.now()-e.lastStatus>10
-        }}
-        await db.collection('participantes').deleteMany(query)
-        client.close()
+        const tempo=Date.now()-10000
+        const search=await db.collection('participantes').find({ lastStatus: { $lte:tempo} }).toArray()
+        if(search.length>0){
+            const mensagemSaida= search.map(e=>{
+                return {
+                    from:e.name,
+                    to:'Todos',
+                    text:`sai da sala...`,
+                    type:'status',
+                    time:dayjs().format('HH:mm:ss')
+                }
+            })
+            db.collection("mensagens").insertMany(mensagemSaida)
+            db.collection('participantes').deleteMany({ lastStatus: { $lte:tempo} })
+        }
     } catch(e){
-        res.sendStatus(500)
-        client.close()
+        res.status(500).send(e)
     }
 }, 15000);
 
